c74ee0edb8948439a31e1926b104110e904f7f40
Катя <katayU@yandex.ru>
2019-05-24 12:02:03 +0300

    Enumerator

--- src/GameResults.java
+++ src/GameResults.java
@@ -1,27 +1,4 @@
-public class GameResults {
-    private String result;
-    public GameResults(double a, double b){
-        if (a == b && a == 0){
-            result = "Dead heat game..";
-        }
-        else if (a == b && a == 1){
-            result = "All players have equal results: 1:1";
-        }
-        else if (a == 1 && b == 0){
-            result = "Player 1 win this gameplay with score: 1:0";
-        }
-        else if (a == 0 && b == 1){
-            result = "Player 2 win this gameplay with score: 1:0";
-        }
-        else if (a == 0.5 && a == b ){
-            result = "Equal results: 1/2 : 1/2";
-        }
-        else
-            result = "You enter not right params for game score, try to replay! :)";
-
+public enum GameResults {
+    DEAD_HEAT,FIRST_WIN,SECOND_WIN,ALL_WIN,HALF_WIN
     }
 
-    public String getResult() {
-        return result;
-    }
-}
