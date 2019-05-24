33aeb9abcc4bccece36e8fc9be3dfdd8471b5cc9
Катя <katayU@yandex.ru>
2019-05-24 12:10:38 +0300

    Enumerator 1.2

--- src/GameResults.java
+++ src/GameResults.java
@@ -1,4 +1,13 @@
 public enum GameResults {
-    DEAD_HEAT,FIRST_WIN,SECOND_WIN,ALL_WIN,HALF_WIN
+    DEAD_HEAT("0:0"),FIRST_WIN("1:0"),SECOND_WIN("0:1"),ALL_WIN("1:1"),HALF_WIN("1/2 : 1/2");
+    
+    String code;
+    GameResults(String s) {
+        this.code = s;
     }
+
+    public String getCode() {
+        return code;
+    }
+}
 
