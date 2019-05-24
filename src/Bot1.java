ee9413379890b775612b994d2e6f9c78d9f857c4
Катя <katayU@yandex.ru>
2019-05-24 13:17:07 +0300

    Katya_Bot 3.0 ( more productive winner, but it is just a lot of ifs and no some any logic)

--- src/Bot1.java
+++ src/Bot1.java
@@ -22,33 +22,122 @@ public class Bot1 extends Bot {
         ActionTicTacToe my_step = new ActionTicTacToe();
         my_step.symbol = mySign;
 
-        char[] action = matrixSmall(currentState);
-        int temp;
-        temp = isWinner(action,opponentSign);   // firstly check if opponent has the winner field and try to remove it;
 
+        char[] action = matrixSmall(currentState);
+        int [] emptyFields = emptyIndices(action);
+        int temp;
+
+        temp = isWinner(action,opponentSign);   // firstly check if opponent has the winner field and try to remove it;
         if (temp != - 1){
             my_step.position = temp;
             return my_step;
 
         }
-
 
         temp = isWinner(action,mySign); //2ly check if I have the winner field and put my character at that place
-
         if (temp != - 1){
             my_step.position = temp;
             return my_step;
         }
-        else {                                              // if no winners just do random
-            temp = getRandom(emptyIndices(action));
+        else {                                              // if no winners just try to find winner strategy steps
+            temp = winnerStrategyCheck(emptyFields,action);
             my_step.position = temp;
             return my_step;
         }
 
-        
+    }
+     /*
+        this method checks existing of winner strategy
+      */
+    private int winnerStrategyCheck(int [] emptyFields, char [] fullField){
+        int temp = getRandom(emptyFields);
+        if (emptyFields.length == fullField.length){
+            temp = (int)Math.round( Math.random()*4) - 1;
+            if (temp == 1)
+                temp = 2;
+            else if (temp == 2)
+                temp = 6;
+            else if (temp == 3)
+                temp = 8;
+            return temp;
+        }
+        if (emptyFields.length == fullField.length - 1){
+            if (fullField[4] == opponentSign || fullField[1] == opponentSign || fullField[3] == opponentSign || fullField[5] == opponentSign || fullField[7] == opponentSign)
+                temp = (int)Math.round( Math.random()*4);
+            else {
+                if (fullField[0] == opponentSign)
+                    temp = 8;
+                else if (fullField[2] == opponentSign)
+                    temp = 6;
+                else if (fullField[6] == opponentSign)
+                    temp = 2;
+                else
+                    temp = 0;
+            }
+            return temp;
+        }
+        if (emptyFields.length == fullField.length - 2){
+            if (fullField[4] == opponentSign){
+                if (fullField[0] == mySign)
+                    temp = 8;
+                else if (fullField[2] == mySign)
+                    temp = 6;
+                else if (fullField[6] == mySign)
+                    temp = 2;
+                else if(fullField[8] == mySign)
+                    temp = 0;
+            }
+            if (fullField[1] == opponentSign || fullField[3] == opponentSign || fullField[5] == opponentSign || fullField[7] == opponentSign)
+                temp = 4;
+            if (fullField[0] == opponentSign && fullField[8] != mySign || fullField[8] == opponentSign && fullField[0] != mySign) {
+                if (fullField[2] == mySign)
+                    temp = 6;
+                else
+                    temp = 2;
+            }
+                else if(fullField[2] == opponentSign && fullField[6] != mySign || fullField[6] == opponentSign && fullField[2] != mySign) {
+                if (fullField[0] == mySign)
+                    temp = 8;
+                else temp = 0;
+            }
+        }
 
-
-
+        if (fullField[0] == mySign && fullField[8] == mySign){
+            if (fullField[3] != opponentSign && fullField[6] != opponentSign & fullField[7] != opponentSign)
+                temp = 6;
+                    else
+                        temp = 2;
+                }
+                else if(fullField[2] == mySign && fullField[6] == mySign){
+                    if (fullField[0] != opponentSign && fullField[1] != opponentSign & fullField[3] != opponentSign)
+                        temp = 0;
+                    else
+                        temp = 8;
+                }
+                if (fullField[4] == mySign){
+                    if (fullField[0] == mySign && fullField[3] != opponentSign && fullField[6] != opponentSign && fullField[2] !=opponentSign)
+                        temp = 6;
+                    else if (fullField[0] == mySign && fullField[1] != opponentSign && fullField[2] != opponentSign && fullField[6] !=opponentSign)
+                        temp = 2;
+                    else if (fullField[2] ==mySign){
+                        if (fullField[8] != opponentSign && fullField[5] != opponentSign && fullField[0] != opponentSign)
+                            temp = 8;
+                        else if (fullField[8] != opponentSign && fullField[1] != opponentSign && fullField[0] != opponentSign)
+                            temp = 0;
+                        }
+                     else if (fullField[6] == mySign){
+                        if (fullField[3] != opponentSign && fullField[0] != opponentSign && fullField[8] != opponentSign)
+                            temp = 0;
+                        else if (fullField[7] != opponentSign && fullField[0] != opponentSign && fullField[8] != opponentSign)
+                            temp = 8;
+                    }
+                    else if (fullField[8] == mySign)
+                        if (fullField[6] != opponentSign && fullField[2] != opponentSign && fullField[5]!= opponentSign)
+                            temp = 2;
+                    else  if (fullField[6] != opponentSign && fullField[2] != opponentSign && fullField[7]!= opponentSign)
+                            temp = 6;
+                }
+        return temp;
     }
     /*
     as input takes one dim. array and in random try to guess to which place put new symbol and return empty field choosing in
