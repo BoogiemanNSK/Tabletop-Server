7dac4356b44f6e381733ef75d1440fd2ed0f587c
BoogiemanNSK <calah47@yandex.ru>
2019-05-29 01:56:49 +0300

    Classes I came up with (interfaces should be updated)

--- src/Checkers/ActionCheckers.java
+++ src/Checkers/ActionCheckers.java
@@ -3,4 +3,12 @@ package Checkers;
 import Interfaces.IAction;
 
 public class ActionCheckers implements IAction {
+
+    // Position of checker that is chosen to be moved
+    Integer checkerX, checkerY;
+
+    // Positions that chosen checker will move to
+    // It is an array since checker can visit several places per turn
+    Integer[] destX, destY;
+
 }
--- src/Checkers/GameStateCheckers.java
+++ src/Checkers/GameStateCheckers.java
@@ -4,13 +4,35 @@ import Interfaces.IGameState;
 
 public class GameStateCheckers implements IGameState {
 
-    @Override
-    public char[][] GameField() {
-        return new char[0][];
+    private char [][] gameField;
+
+    // Here r is standard red checker, b is standard black checker
+    // R would be red queen and B - black queen
+    // Other fields are underline symbol (empty)
+    public GameStateCheckers(){
+        this.gameField = new char[8][8];
+
+        this.gameField[0] = new char[]{'_', 'r', '_', 'r', '_', 'r', '_', 'r'};
+        this.gameField[1] = new char[]{'r', '_', 'r', '_', 'r', '_', 'r', '_'};
+        this.gameField[2] = new char[]{'_', 'r', '_', 'r', '_', 'r', '_', 'r'};
+        this.gameField[3] = new char[]{'_', '_', '_', '_', '_', '_', '_', '_'};
+        this.gameField[4] = new char[]{'_', '_', '_', '_', '_', '_', '_', '_'};
+        this.gameField[5] = new char[]{'b', '_', 'b', '_', 'b', '_', 'b', '_'};
+        this.gameField[6] = new char[]{'_', 'b', '_', 'b', '_', 'b', '_', 'b'};
+        this.gameField[7] = new char[]{'b', '_', 'b', '_', 'b', '_', 'b', '_'};
+    }
+
+    public char[][] GameField(){
+        return gameField;
     }
 
     @Override
     public void showField() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++)
+                System.out.print(gameField[i][j] + " ");
+            System.out.println();
+        }
     }
 
 }
