e06bea5d0d3f9f137ba772a3daebc44164717e5d
Luiz Araujo <ljonata@gmail.com>
2019-06-06 15:33:23 +0300

    Basic implementation for Bot1

--- src/Checkers/Bot1.java
+++ src/Checkers/Bot1.java
@@ -1,237 +1,62 @@
+package Checkers;
+
+import java.util.LinkedList;
 import java.util.Random;
+
+import Checkers.GameStateCheckers.Position;
+import Checkers.GameStateCheckers.Token;
+import Interfaces.Bot;
+import Interfaces.IAction;
+import Interfaces.IGameState;
+
 public class Bot1 extends Bot {
 
-    private char mySign;
-    private Random rnd;
-    ActionCheckers token;
+	private char mySign;
+	private Random rnd;
+	ActionCheckers token;
 
-    public Bot1(int botId) {
-        super(botId);
+	public Bot1(int botId) {
+		super(botId);
+	}
 
-    }
+	@Override
+	public IAction makeDecision(final IGameState currentState) {
+		GameStateCheckers stateCheckers = (GameStateCheckers) currentState;
+		// Two parameters to create an action
+		LinkedList<Position> moves = new LinkedList<>();
+		Token bestToken = null;
 
-    public IAction makeDecision(final IGameState currentState) {
+		// Deciding what is our best token
+		for (Token token : stateCheckers.getAllTokens()) {
+			if (token.player.id == id)
+				if (bestToken == null)
+					bestToken = token;
+				else {
+					if (token.position.row > bestToken.position.row)
+						bestToken = token;
+					else if (token.position.row == bestToken.position.row
+							& token.position.column < bestToken.position.column)
+						bestToken = token;
+				}
+		}
+		// Deciding the best move to the selected token
+		
+		// validating the row above
+		if (bestToken.position.row + 1 >= 0 & bestToken.position.row + 1 <= stateCheckers.MAX_ROW) {
+			
+		}
 
+		Position p1 = stateCheckers.getPosition(bestToken.position.row + 1, bestToken.position.column - 1);
+		Position p2 = stateCheckers.getPosition(bestToken.position.row + 1, bestToken.position.column + 1);
+		Position move = null;
+		if (stateCheckers.getToken(p1) != null) {
+			move = p1;
+		} else {
+			move = p2;
+		}
+		moves.add(move);
 
-        String ally = "ally";
-        String Qally = "queenally";
-        String empty = "empty";
-        String enemy = "enemy";
-        GameStateCheckers gameState = (GameStateCheckers) currentState;
-        String[][] Field = gameState.GameField();
-        String[][] mas = new String[8][8];
-        int[] pos0 = new int[12];
-        int[] pos1 = new int[12];
-        int[] pos2 = new int[12];
-        int[] pos3 = new int[12];
-       // int[] pos4 = new int[12];
-
-        for (int i = 0; i < 12; i++) {
-            pos0[i] = -1;
-            pos1[i] = -1;
-            pos2[i] = -1;
-            pos3[i] = -1;
-            //pos4[i] = -1;
-        }
-
-        //Character[] lineState = new Character[9];
-        int k = 0;
-        for (int i = 0; i < 8; i ++)
-            for ( int j = 0; j < 8; j++) {
-                mas[i][j] = gameState.GameField()[i][j];
-                if(Field[i][j] == ally) {
-                    //for pos 0
-                    if (i > 0 && j > 0) {
-                        if (Field[i - 1][j - 1] == empty) {
-                            for (int l = 0; l < 12; l++) {
-                                if (pos0[l] == i * 10 + j)
-                                    break;
-                                else {
-                                    if (pos0[l] == -1) {
-                                        pos0[l] = i * 10 + j;
-                                        break;
-                                    }
-                                }
-                            }
-                            if (i > 1 && j > 1){
-                                if (Field[i - 2][j - 2] != enemy && (Field[i][j - 2] != enemy && Field[i - 2][j] != empty)) {
-                                    for (int l = 0; l < 12; l++) {
-                                        if (pos1[l] == i * 10 + j)
-                                            break;
-                                        else {
-                                            if (pos1[l] == -1) {
-                                                pos1[l] = i * 10 + j;
-                                                break;
-                                            }
-                                        }
-                                    }
-                                }
-                        }
-                        }
-                        if (Field[i - 1][j - 1] == enemy && Field[i - 2][j - 2] == empty){
-                            for (int l = 0; l < 12; l++) {
-                                if(pos2[l] == i * 10 + j)
-                                    break;
-                                else {
-                                    if (pos2[l] == -1) {
-                                        pos2[l] = i * 10 + j;
-                                        break;
-                                    }
-                                }
-                            }
-
-                        }
-                        if(i > 1 && j < 6){
-                            if(Field[i - 1][j + 1] == enemy && Field[i - 2][j - 2] == empty){
-                                for (int l = 0; l < 12; l++) {
-                                    if(pos2[l] == i * 10 + j)
-                                        break;
-                                    else { if (pos2[l] == -1) {
-                                        pos2[l] = i * 10 + j;
-                                        break;
-                                    }
-                                    }
-                                }
-                            }
-                        }
-                        if(i > 3 && j > 3) {
-                            if (Field[i - 3][j - 3] != enemy && Field[i - 1][j - 3] != enemy && Field[i - 3][j - 1] != empty) {
-                                for (int l = 0; l < 12; l++) {
-                                    if(pos3[l] == i * 10 + j)
-                                        break;
-                                    else{if(pos3[l] == -1) {
-                                        pos3[l] = i * 10 + j;
-                                        break;
-                                    }
-                                    }
-
-                                }
-                            }
-                        }
-                    }
-                    if (i < 7 && j < 7) {
-                        if (Field[i + 1][j - 1] == empty) {
-                            for (int l = 0; l < 12; l++) {
-                                if (pos0[l] == i * 10 + j)
-                                    break;
-                                else {if (pos0[l] == -1) {
-                                    pos0[l] = i * 10 + j;
-                                    break;
-                                }
-                                }
-
-                            }
-                            if (i < 6 && j < 6) {
-                                if (Field[i + 2][j - 2] != enemy && (Field[i][j - 2] != enemy && Field[i + 2][j] != empty)) {
-                                    for (int l = 0; l < 12; l++) {
-                                        if (pos1[l] == i * 10 + j)
-                                            break;
-                                        else{ if (pos1[l] == -1) {
-                                            pos1[l] = i * 10 + j;
-                                            break;
-                                        }
-                                        }
-
-                                    }
-                                }
-                            }
-                        }
-                        if (i > 1 && j > 1){
-                            if (Field[i + 1][j - 1] == enemy && Field[i + 2][j - 2] == empty) {
-                                for (int l = 0; l < 12; l++) {
-                                    if (pos2[l] == i * 10 + j)
-                                        break;
-                                    else{ if(pos2[l] == -1) {
-                                        pos2[l] = i * 10 + j;
-                                        break;
-                                    }
-                                    }
-
-                                }
-                                if(i < 5 && j > 2)
-                                if (Field[i + 1][j - 3] != enemy && Field[i + 3][j - 1] != empty && Field[i + 3][j - 3] != enemy) {
-                                    for (int l = 0; l < 12; l++) {
-                                        if(pos3[l] == i * 10 + j)
-                                            break;
-                                        else {if (pos3[l] == -1) {
-                                            pos3[l] = i * 10 + j;
-                                            break;
-                                        }
-                                        }
-                                    }
-                                }
-
-                            }
-                    }
-                        if(i < 6 && j < 6){
-                            if(Field[i + 1][j + 1] == enemy && Field[i + 2][j + 2] == empty){
-                                for (int l = 0; l < 12; l++) {
-                                    if(pos2[l] == i * 10 + j)
-                                        break;
-                                    else {if(pos2[l] == -1) {
-                                        pos2[l] = i * 10 + j;
-                                        break;
-                                    }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                if(Field[i][j] == Qally)
-                    for (int l = 0; l < 12; l++) {
-                        if(pos0[l] == -1) {
-                            pos0[l] = i * 10 + j;
-                            break;
-                        }
-
-                    }
-
-
-
-            }
-        int range3 = -1;
-        int range2 = -1;
-        int range1 = -1;
-        int range0 = -1;
-            for (int i = 0; i < 12; i++) {
-                if(pos0[i] != -1 )
-                    range0 = i;
-                if(pos1[i] != -1)
-                    range1 = i;
-                if(pos2[i] != -1)
-                    range2 = i;
-                if(pos3[i] != -1)
-                    range3 = i;
-            }
-            if(range3 != -1){
-                int a = (int)(Math.random()*++range3);
-                if (Field[pos3[a]%10 - 1][pos3[a]/10 - 1] == empty)
-                System.out.println(pos3[a]);
-                else{
-                    System.out.println(Field[pos3[a]%10 + 1][pos3[a]/10 - 1]);
-                }
-
-            }else {
-                if (range2 != -1) {
-                    int a =(int) (Math.random() * ++range2);
-                    System.out.println(pos2[a]);
-                } else {
-                    if (range1 != -1) {
-                        int a = (int) (Math.random() * ++range1);
-                        System.out.println(pos1[a]);
-                    } else {
-                        if (range0 != -1) {
-                            int a = (int) (Math.random() * ++range0);
-                            System.out.println(pos0[a]);
-                        }
-                    }
-                }
-            }
-
-        ActionCheckers action = new ActionCheackers();
-        return action;
-    }
-
+		return new ActionCheckers(bestToken, moves);
+	}
 
 }
